f573e40231172f2460ea3a4ecc69f70f21eb5805
josephndegwa <kariukindegwa98@gmail.com>
2022-12-01 14:39:47 +0300

    first commit

---
+++ src/components/PageTitle/PageTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>
+        {{ title }}
+    </h1>
+</template>
+
+<script>
+    export default {
+        name: 'PageTitle',
+        props: {
+            title: String,
+            required: true
+        }
+    }
+</script>
+
+<style>
+    .page-title {
+        margin-bottom: 2rem;
+        color: aqua;
+        text-align: center;
+        font-weight: 700;
+    }
+</style>
